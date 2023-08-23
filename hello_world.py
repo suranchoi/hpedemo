d45e88c83d4cb40104f3ff6e0c2053ddd80cb389
suran.choi <tnfks0217@gmail.com>
2023-08-23 15:15:54 +0900

    test_dag

---
+++ hello_world.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+
+from airflow import DAG
+
+# Operator
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': days_ago(2),
+    # 'email': ['airflow@example.com'],
+    # 'email_on_failure': False,
+    # 'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    # 'queue': 'bash_queue',
+    # 'pool': 'backfill',
+    # 'priority_weight': 10,
+    # 'end_date': datetime(2016, 1, 1),
+    # 'wait_for_downstream': False,
+    # 'dag': dag,
+    # 'sla': timedelta(hours=2),
+    # 'execution_timeout': timedelta(seconds=300),
+    # 'on_failure_callback': some_function,
+    # 'on_success_callback': some_other_function,
+    # 'on_retry_callback': another_function,
+    # 'sla_miss_callback': yet_another_function,
+    # 'trigger_rule': 'all_success'
+}
+
+
+dag = DAG(
+    'helloworld',
+    default_args=default_args,
+    description='echo "hello, world!"',
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = BashOperator(
+    task_id='echo_hello_world',
+    bash_command='echo "Hello, World!"',
+    dag=dag,
+)
