506536b7f7046077fbd2e79879ef4eccfda6b074
suran.choi <tnfks0217@gmail.com>
2023-09-05 16:06:35 +0900

    remove old files

--- dags/spark_mnist.py
+++ dags/spark_mnist.py
@@ -1,6 +1,6 @@
 from airflow import DAG
+from airflow.models.param import Param
 from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
-from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -11,38 +11,28 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'max_active_runs': 1,
-    'retries': 3
+    'retries': 0
 }
 
 dag = DAG(
-    'spark_read_write_parquet_mnist',
+    'test_spark_read_write_parquet_mnist',
     default_args=default_args,
     schedule_interval=None,
     tags=['e2e example', 'ezaf', 'spark', 'parquet', 'mnist'],
     params={
-        'username': "admin",
-        'training_path': "mnist-spark-data",
-        's3_secret_name': "spark-s3-creds"
-    }
+        'username': Param("admin", type="string"),
+        'training_path': Param("mnist-spark-data", type="string"),
+        's3_secret_name': Param("spark-s3-creds", type="string"),
+        'airgap_registry_url': Param("", type=["null", "string"], pattern=r"^$|^\S+/$")
+    },
+    render_template_as_native_obj=True
 )
 
 submit = SparkKubernetesOperator(
-    task_id='ezaf_spark_mnist_submit',
+    task_id='submit',
     namespace="spark",
-    application_file="example_ezaf_spark_mnist.yaml",
-    do_xcom_push=True,
+    application_file="ezaf_spark_mnist.yaml",
     dag=dag,
     api_group="sparkoperator.hpe.com",
     enable_impersonation_from_ldap_user=True
 )
-
-sensor = SparkKubernetesSensor(
-    task_id='ezaf_spark_mnist_s3_creds_secret_monitor',
-    namespace="spark",
-    application_name="{{ task_instance.xcom_pull(task_ids='ezaf_spark_mnist_submit')['metadata']['name'] }}",
-    dag=dag,
-    api_group="sparkoperator.hpe.com",
-    attach_log=True
-)
-
-submit >> sensor
